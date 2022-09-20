aab752c9c18f7a656ffce89d830a8c896b79c9e8
Cherry013 <charan172003@gmail.com>
2022-09-20 13:44:01 +0530

    Backend Deleted

--- Palnadu/BackEnd/index.js
+++
@@ -1,6 +0,0 @@
-const app = require('./Requires');
-const connection = require('./connection');
-const port = process.env.PORT || 3000;
-app.listen(port,()=>{
-    console.log(`Listening on port ${port}`);
-})
