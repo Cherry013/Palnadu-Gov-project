e94b6a22bb5788e26c5ff2579f83fa06cd55a6cb
Cherry013 <charan172003@gmail.com>
2022-09-21 10:45:25 +0530

    Pie Chart Modified

--- Palnadu/PalnaduDashboard/src/app/charts-c/charts-c.component.ts
+++ Palnadu/PalnaduDashboard/src/app/charts-c/charts-c.component.ts
@@ -12,20 +12,22 @@ export class ChartsCComponent implements OnInit {
   public legendSettings: object | undefined;
   public tooltip : object | undefined;
   public datalabel: Object = { visible:true, position:'inside', name:'text' };
+  public palette: String[] = [];
   ngOnInit(): void {
     this.piedata=[
-      {x:'Begging',y:'{{Begging}}'},{x:'Hotel',y:'{{Hotels}}'},
-      {x:'shop/pvt-Sector',y:'{{pvt}}'},{x:'Railway Station',y:'{{rail}}'},
+      {x:'Begging',y:15},{x:'Hotel',y:25},
+      {x:'shop/pvt-Sector',y:35},{x:'Railway Station',y:'{{rail}}'},
       {x:'Brick klins',y:'{{brick}}'},{x:'Factories',y:'{{Factories}}'},{x:'Constrction Workers',y:'{{Construction}}'},
       {x:'Others',y:'{{other}}'}
     ];
-    this.legendSettings = { vsible:true };
+    this.legendSettings = { vsible:false };
     this.tooltip = { enable:true,
       format: '${series.name} ${point.x} : ${point.y}' ,
       fill: '#000',
       border:{
         color:'black'
       }
-    }
+    };
+    this.palette = ["#29c2ac", "#fe9496", "#4bcbeb"]
   }
 }
